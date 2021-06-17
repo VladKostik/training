3f81f8961cf90d185b9a7888dfe87d456d0ae86b
VladKostik <udsa.staff@gmail.com>
2021-06-17 12:44:11 +0300

    ДЗ к занятию от 15.06.2021 Drill #5 List of friends

---
+++ t_5.py
@@ -0,0 +1,28 @@
+#5 List of friends
+
+john_friends = ['Andrew', 'Simon', 'Jacy']
+marta_friends = ['Arthur', 'Sarah', 'Mary']
+
+#Output
+common_friends = john_friends + marta_friends
+
+def who_are_friends(common_friends):
+    print('Tape name: ')
+    name = input()
+
+    if name == 'John':
+        i = 0
+        while i <= 2:
+            print(f'{common_friends[i]} is {name}`s friend')
+            i += 1
+
+    elif name == 'Marta':
+        i = 3
+        while i != 6:
+            print(f'{common_friends[i]} is {name}`s friend')
+            i += 1
+    else:
+        print(f'Have no idea who are {name}`s friends')
+
+who_are_friends(common_friends)
+
