c7cdd41fe2ecf8bdeef6ef5b42721f2965c8a47d
VladKostik <udsa.staff@gmail.com>
2021-06-27 18:03:14 +0300

    ID: HW3 * Done

---
+++ homework_3/module_3_2.py
@@ -0,0 +1,20 @@
+# У вас есть цыфра 2 в качестве перменной. Умножте ее на 2 двумя способами.
+# Соответственно нужно ее и поделить 2 разными способами на 2.
+
+number = 2
+
+# First multiplication
+m_1 = number*2
+print(m_1)
+
+# Second multiplication
+m_2 = number << 1
+print(m_2)
+
+# First division
+d_1 = number/2
+print(d_1)
+
+# Second division
+d_2 = number >> 1
+print(d_2)
