16e46360e5e90984c9248315f0009c5596c53722
VladKostik <udsa.staff@gmail.com>
2021-08-06 20:41:27 +0300

    Revert "Delete homework_9 directory"
    This reverts commit 08c09f3f

--- homework_9/dir/dir1/__init__.py
+++ homework_9/dir/dir1/__init__.py
@@ -1,4 +1,8 @@
 from math1 import add, dimm
+<<<<<<< HEAD
 from math2 import divide, mult
 
 # Good.
+=======
+from math2 import divide, mult
+>>>>>>> parent of 08c09f3... Delete homework_9 directory
