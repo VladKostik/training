bc387158a3e44502082299e48a00b551e8e79a56
VladKostik <udsa.staff@gmail.com>
2021-08-06 20:40:45 +0300

    Revert "Delete homework_1 directory"
    This reverts commit c3c3742ac67f9cb9aabdb38920e83d74a4148a83.

---
+++ homework_1/t_1.py
@@ -0,0 +1,12 @@
+# 1 Salary print
+
+john_salary = 1500.5
+marta_salary = 1111.6
+
+print(john_salary)
+
+print(marta_salary)
+
+print(marta_salary)
+
+# Good but too much new lines in file after code
---
+++ homework_1/t_2.py
@@ -0,0 +1,12 @@
+# 2 Age print
+
+john_age = 33
+marta_age = 28
+
+print(john_age)
+
+print(marta_age)
+
+print(marta_age)
+
+# Good but 2 new lines after code was present
