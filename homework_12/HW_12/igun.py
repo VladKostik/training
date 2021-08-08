ba13b9925b566ddc534225db2d7bab07d0c8e83b
VladKostik <udsa.staff@gmail.com>
2021-08-08 23:36:45 +0300

    ID: HW12 * Done

---
+++ homework_12/HW_12/igun.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+
+class IGun(ABC):
+    @abstractmethod
+    def shooting(self):
+        """Describes shooting the gun"""
+        pass
+
+    @abstractmethod
+    def loading(self):
+        """Describes loading the gun"""
+        pass
