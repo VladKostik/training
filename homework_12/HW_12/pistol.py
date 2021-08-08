547bb1c0c3927e0f7739e41ba9b5b7177009319b
VladKostik <udsa.staff@gmail.com>
2021-08-09 00:42:23 +0300

    ID: HW12 * Changed loading and shooting methods

--- homework_12/HW_12/pistol.py
+++ homework_12/HW_12/pistol.py
@@ -18,7 +18,7 @@ class Pistol(IGun):
     @property
     def loading(self):
         """Returns pistol status - loaded or unloaded"""
-        if self._current_number_of_rounds >= 0:
+        if self._current_number_of_rounds <= 0:
             self.__is_loaded = False
             return f"The {self.__name} is unloaded"
         else:
@@ -29,13 +29,14 @@ class Pistol(IGun):
     @loading.setter
     def loading(self, rounds: int) -> None:
         """Sets current number of rounds in pistol"""
-        if rounds > self.__mag_capacity:
-            raise ValueError
+        if rounds > self.__mag_capacity or rounds < 0:
+            raise ValueError(f'Number of rounds should be '
+                             f'between 1 and {self.__mag_capacity}')
         self._current_number_of_rounds = rounds
 
     def shooting(self):
         """Describes shooting the pistol"""
-        if self._current_number_of_rounds > 0 or self.__is_loaded == True:
+        if self._current_number_of_rounds > 0:
             self._current_number_of_rounds -= 1
             return f'{self.__name} shot one round'
         else:
