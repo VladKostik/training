e5650e2e76705feeb184f1de647ef11301544aef
VladKostik <udsa.staff@gmail.com>
2021-08-18 22:58:00 +0300

    Revert "Delete homework_2 directory"
    This reverts commit b08b24c96ba34c1497963125acc0df38f2aefc56.

---
+++ homework_2/module_3.py
@@ -0,0 +1,22 @@
+# У вас есть две группы людей.
+# В одной группе состоят всеядные люди в другой лишь вегетарианцы.
+# Всеядный является вегетарианцем но вегетарианец не является всеядным.
+# Вывести список гостей которые могут есть овощи и зелень.
+
+all_eaters = ['Jeck Lincoln', 'Robert Smith', 'Jerry Miculek', 'Antony Hopkins']
+vegetarians = ['Adolf Schiklgruber', 'Mahatma Gandhi', 'Oleg Popov']
+
+vegetarians.extend(all_eaters)
+print('Guests who can eat vegetables:')
+i = 1
+for name in vegetarians:
+    print(f'{i}. {name}')
+    i += 1
+
+# Good. Nice solution but take a look how you can make it better and easier.
+# print(all_eaters + vegetarians)
+#
+# 1 mistak you have extend vbegeterians with all eaters
+# you should not update origin groups of people
+# just create additional list which will contain all of them from both lists
+
