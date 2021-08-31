6d9bd1539a1c21c3680e5a0daf0fceb362213d37
VladKostik <86021794+VladKostik@users.noreply.github.com>
2021-08-31 15:39:32 +0300

    Delete homework_15 directory

--- homework_15/HW_factory/__init__.py
+++
@@ -1,4 +0,0 @@
-from homework_15.HW_factory.iproduct import IProduct
-from homework_15.HW_factory.apple import Apple
-from homework_15.HW_factory.pear import Pear
-from homework_15.HW_factory.potato import Potato
