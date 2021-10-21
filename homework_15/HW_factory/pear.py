4b912fde792436770a53bbb65f727b583d2cc8c4
VladKostik <86021794+VladKostik@users.noreply.github.com>
2021-10-21 13:41:12 +0300

    Delete homework_15 directory

--- homework_15/HW_factory/pear.py
+++
@@ -1,5 +0,0 @@
-from .iproduct import IProduct
-
-
-class Pear(IProduct):
-    _product_name = 'pear'
--- homework_15/HW_singleton/__init__.py
+++
@@ -1,2 +0,0 @@
-from homework_15.HW_singleton.singleton_akm import singleton_akm
-from homework_15.HW_singleton.akm import AKM
