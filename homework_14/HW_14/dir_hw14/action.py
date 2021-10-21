385db885ff90554d30b18e8ea374032515da4ad2
VladKostik <86021794+VladKostik@users.noreply.github.com>
2021-10-21 13:41:03 +0300

    Delete homework_14/HW_14 directory

--- homework_14/HW_14/dir_hw14/action.py
+++
@@ -1,8 +0,0 @@
-class Action:
-    def __init__(self, action_name: str):
-        self.action_name = action_name
-
-    def action(self):
-        return self.action_name
-
-# Action class should be functor but not simple class -2 points
